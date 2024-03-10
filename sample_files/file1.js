c20a0465eaacf5e5bc745079ceed7bf4b572425d
VatsalShashwat <vatsalshashwat@gmail.com>
2024-03-10 11:37:26 +0530

    Add files via upload

---
+++ sample_files/file1.js
@@ -0,0 +1,7 @@
+// Sample JavaScript file 1
+const apiKey = 'YOUR_API_KEY';
+const username = 'admin';
+const password = 'password123';
+const apiUrl = 'https://api.example.com';
+
+
---
+++ sample_files/file2.js
@@ -0,0 +1,7 @@
+
+
+// Sample JavaScript file 2
+const accessToken = 'YOUR_ACCESS_TOKEN';
+const apiUrl = 'https://api.example.com/v1';
+const sensitiveUrl = 'https://example.com/login';
+
